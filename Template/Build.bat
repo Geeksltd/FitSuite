2781604c028b1d49629e584c6636cd213228d545
Matt <matt@geeks.ltd.uk>
2021-07-25 14:26:38 +0100

    no message

--- Template/Build.bat
+++ Template/Build.bat
@@ -1,10 +1,7 @@
 @echo off
 
-call dotnet tool install -g msharp-build
-call msharp-build %1 %2 %3 %4 %5
-
-if ERRORLEVEL 1 (    
-	echo ##################################    
-    set /p cont= Error occured. Press Enter to exit.
-    exit /b -1
-)
+cd Website
+call yarn
+call tsc
+call pushd wwwroot\styles\build\ && .\sass-to-css.bat && popd
+call dotnet build
---
+++ Template/Website/app_Start/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class TaskManager : Olive.BackgroundJobsPlan
+{
+    public TaskManager()
+    {
+
+    }
+
+    public override void Initialize()
+    {
+        // TODO
+    }
+}
